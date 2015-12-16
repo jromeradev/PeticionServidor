dc9cabbb54a1c753458e26a953d01e69da486498
Carolina Ramirez Toledo <carolinart@gmail.com>
2015-12-17 00:28:23 +0100

    Initial Commit

---
+++ PeticionServidor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeticionServidor
+//
+//  Created by Jose on 17/12/15.
+//  Copyright © 2015 jromeradev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

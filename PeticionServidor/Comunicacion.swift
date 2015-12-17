0e6f20a837a713dd69fb002eab05a6425c741a06
Carolina Ramirez Toledo <carolinart@gmail.com>
2015-12-17 23:47:45 +0100

    Interacción

---
+++ PeticionServidor/Comunicacion.swift
@@ -0,0 +1,23 @@
+//
+//  Comunicacion.swift
+//  PeticionServidor
+//
+//  Created by Jose on 17/12/15.
+//  Copyright © 2015 jromeradev. All rights reserved.
+//
+
+import Foundation
+
+class Comunicacion {
+    
+    let urlBase:String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+    
+    func loadBookInfo(isbn:String)->NSString?{
+        let url = NSURL(string:urlBase+isbn)
+        let datos:NSData? = NSData(contentsOfURL:url!)
+        let texto:NSString? = NSString(data:datos!, encoding: NSUTF8StringEncoding)
+        return texto
+    }
+    
+   
+}
--- PeticionServidor/ViewController.swift
+++ PeticionServidor/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    var com:Comunicacion = Comunicacion()
+    @IBOutlet weak var result: UITextView!
+    @IBOutlet weak var isbn: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.isbn.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clear(sender: AnyObject) {
+        isbn.text=""
+        result.text=""
+    }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+       
+        do {
+         let data:NSString? = try com.loadBookInfo(isbn.text!)
+        if (data != nil){
+            result.text = (data as String?)!
+        }
+        }catch let ex as NSException {
+            result.text = ex.callStackSymbols.joinWithSeparator("-")
+        }
+                
+        return true
+    }
+    
 }
 

47dbcd70948c64eb449f5a58e0815feca3451b5c
Carolina Ramirez Toledo <carolinart@gmail.com>
2015-12-31 00:36:51 +0100

    Parse JSON

--- PeticionServidor/ViewController.swift
+++ PeticionServidor/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController,UITextFieldDelegate {
     
     let urlBase:String = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
     
-    @IBOutlet weak var result: UITextView!
     @IBOutlet weak var isbn: UITextField!
+    
+    @IBOutlet weak var titulo: UILabel!
+    
+    @IBOutlet weak var autores: UILabel!
+    
+    @IBOutlet weak var portada: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +33,9 @@ class ViewController: UIViewController,UITextFieldDelegate {
 
     @IBAction func clear(sender: AnyObject) {
         isbn.text=""
-        result.text=""
+        titulo.text = ""
+        autores.text = ""
+        portada.image = nil
     }
 
     func loadBookInfo(isbn:String)throws->NSString? {
@@ -41,12 +48,49 @@ class ViewController: UIViewController,UITextFieldDelegate {
         return texto
     }
     
+    func loadBookData(isbn:String)throws->NSData? {
+        let url = NSURL(string:urlBase+isbn)
+        let datos:NSData? = try NSData(contentsOfURL:url!,options: NSDataReadingOptions.DataReadingMappedIfSafe)
+        return datos
+    }
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         do{
-            let data:NSString? = try loadBookInfo(isbn.text!)
-            if (data != nil){
-                result.text = (data as String?)!
+            //let data:NSString? = try loadBookInfo(isbn.text!)
+            //if (data != nil){
+            let datos:NSData? = try loadBookData(isbn.text!)
+            if (datos != nil) {
+                do{
+                    let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                    if let mainjson = json as? NSDictionary{
+                        if let main = mainjson["ISBN:"+isbn.text!] as? NSDictionary {
+                            let titlejs = main["title"] as! NSString as String
+                            titulo.text = titlejs
+                            let autoresjs = main["authors"] as! NSArray
+                    
+                            if let coverMainjs = main["cover"] as? NSDictionary {
+                    
+                                let urlCoverjs = coverMainjs["medium"] as! NSString as String
+                                let url = NSURL(string:urlCoverjs)
+                                let dataImg = NSData(contentsOfURL: url!)
+                                portada.image = UIImage(data:dataImg!)
+                                
+                            }
+                            for autor in autoresjs {
+                                autores.text = autores.text! + (autor["name"] as! NSString as String) + ","
+                            }
+                        }
+                    }
+                    
+                    
+                }catch let error as NSError {
+                    let alerta = UIAlertController(title:"Error acceso datos", message: error.description, preferredStyle:.Alert)
+                    let OKAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alerta.addAction(OKAction)
+                    self.presentViewController(alerta, animated: true, completion: nil)
+                }
+                
             }
         } catch let error as NSError {
             let alerta = UIAlertController(title:"Error acceso URL", message: error.description, preferredStyle:.Alert)
